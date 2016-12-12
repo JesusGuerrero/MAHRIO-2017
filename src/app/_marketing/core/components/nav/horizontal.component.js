b7c6e6715424a79f2a5c3a8b60819e902ace2ba1
Jesus Guerrero <rocha.jesus@hotmail.com>
2016-12-11 20:09:39 -0800

    Add files via upload

---
+++ src/app/_marketing/core/components/nav/horizontal.component.js
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+import template from './horizontal.template.html';
+
+@Component({
+  selector: 'marketing-horizontal-nav',
+  template
+})
+
+export class MarketingHorizontalNavComponent {}
---
+++ src/app/core/components/nav/horizontal.component.js
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+import template from './horizontal.template.html';
+
+@Component({
+  selector: 'horizontal-nav',
+  template
+})
+
+export class HorizontalNavComponent {}
